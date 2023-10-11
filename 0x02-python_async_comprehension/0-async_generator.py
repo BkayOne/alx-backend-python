b8f3391d66f6074c8094afbf612b82244177e8fc
Frankelly Ozonwu <chiedoziefrankelly@gmail.com>
2023-10-11 09:31:15 +0000

    Async Generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""Async Generator"""
+
+from random import uniform
+from typing import Generator
+import asyncio
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """coroutine will loop 10 times, each time asynchronously wait """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield (uniform(0, 10))
